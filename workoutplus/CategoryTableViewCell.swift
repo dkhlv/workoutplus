d10a81227b3d4b85856a181f7c4ffa767303cf85
DK <dkhlv@users.noreply.github.com>
2020-04-06 23:24:29 -0400

    Initial layout + app icons

---
+++ workoutplus/CategoryTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryTableViewCell.swift
+//  workoutplus
+//
+//  Created by Diana Koval on 2020-04-05.
+//  Copyright © 2020 Diana Koval. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var workoutTypeLabel: UILabel!
+    
+}
