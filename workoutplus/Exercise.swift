91ca11988d0a9f6dbad7517914af2a2088471153
DK <dkhlv@users.noreply.github.com>
2020-04-07 22:17:40 -0400

    add json data source

---
+++ workoutplus/Exercise.swift
@@ -0,0 +1,30 @@
+//
+//  Exercise.swift
+//  workoutplus
+//
+//  Created by Diana Koval on 2020-04-07.
+//  Copyright © 2020 Diana Koval. All rights reserved.
+//
+
+import Foundation
+
+class Exercise {
+    var name: String
+    var id: Int
+    var category: String
+    var sets: String
+    var reps: String
+    var weight: String
+    
+    
+    init(data: JSON)
+    {
+        self.name = "\(data["name"].stringValue)"
+        self.id = data["id"].intValue
+        self.category = "\(data["category"].stringValue)"
+        self.sets = "\(data["sets"].stringValue)"
+        self.reps = "\(data["reps"].stringValue)"
+        self.weight = "\(data["weight"].stringValue)"
+
+    }
+}
---
+++ workoutplus/ExerciseTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ExerciseTableViewCell.swift
+//  workoutplus
+//
+//  Created by Diana Koval on 2020-04-07.
+//  Copyright © 2020 Diana Koval. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var exerciseNameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
